4a62ac7421fe6fa927b4a2a28b7b4e05b6676009
Cheng-An Ma <s84190kimo@yahoo.com.tw>
2018-10-14 22:42:23 -0500

    add init screen and minor changes

--- serviceworker.js
+++ serviceworker.js
@@ -1,55 +1,38 @@
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', function() {
-//     navigator.serviceWorker.register('/ampweb/serviceworker.js').then(function(registration) {
-//       // Registration was successful
-//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//     }, function(err) {
-//       // registration failed :(
-//       console.log('ServiceWorker registration failed: ', err);
-//     });
-//   });
-// }
-
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
-       '/',
-       'index.html',
-       'style.css',
-       '/images/aarp.jpg',
-       '/images/amex-prefer.jpg',
-       '/images/amex-ascend.jpg',
-       '/images/chase.jpg',
-       '/images/discover.jpg'
-     ]);
-   })
- );
-});
-
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static').then(function(cache) {
+      cache.addAll([
+        '/ampweb',
+        'index.html', 
+        'manifest.json',
+        '/ampweb/images/aarp.jpg',
+        '/ampweb/images/amex-prefer.jpg',
+        '/ampweb/images/amex-ascend.jpg',
+        '/ampweb/images/chase.jpg',
+        '/ampweb/images/discover.jpg'
+        ]);
     })
   );
 });
 
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+});
 
-// btnAdd.addEventListener('click', (e) => {
-//   // hide our user interface that shows our A2HS button
-//   btnAdd.style.display = 'none';
-//   // Show the prompt
-//   deferredPrompt.prompt();
-//   // Wait for the user to respond to the prompt
-//   deferredPrompt.userChoice
-//     .then((choiceResult) => {
-//       if (choiceResult.outcome === 'accepted') {
-//         console.log('User accepted the A2HS prompt');
-//       } else {
-//         console.log('User dismissed the A2HS prompt');
-//       }
-//       deferredPrompt = null;
-//     });
-// });
+self.addEventListener('fetch', function(event) {
+  // event.respondWith(
+  //   caches.match(event.request).then(function(response) {
+  //     if (response) {
+  //       return response;
+  //     } else {
+  //       return fetch(event.request).then(function(res) {
+  //         return caches.open('dynamic').then(function(cache) {
+  //           cache.put(event.request.url, res.clone());
+  //           return res;
+  //         });
+  //       });
+  //     }
+  //   })
+  // );
+});
